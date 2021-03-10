54884523c60f324b9b9ff9c77d3717b7c1bb2b8b
ramon.garrido <ramon.garrido@es.gfi.world>
2021-03-10 17:52:50 +0100

    metodo getPricesPerClientNItemsCallable, parametro umv

--- src/main/java/es/inetum/world/HazelcastMediator.java
+++ src/main/java/es/inetum/world/HazelcastMediator.java
@@ -7,12 +7,23 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IExecutorService;
 import com.hazelcast.map.IMap;
 
-import es.fluidra.prices.data.process.GetSinglePricePerItemClientCallable;
-import es.fluidra.prices.data.values.NetProductPrice;
+import es.fluidra.hazelcast.prices.process.task.GetSinglePricePerItemClientCallable;
+import es.fluidra.hazelcast.prices.process.task.GetPricesPerClientNItemsCallable;
+import es.fluidra.hazelcast.prices.data.values.NetProductPriceResponse;
+import es.fluidra.hazelcast.prices.data.keys.BasicTariffKey;
+import es.fluidra.hazelcast.prices.data.values.TariffProductPrice;
+import es.fluidra.hazelcast.prices.data.values.NetProductPrice;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
 
 import org.apache.synapse.ManagedLifecycle;
 import org.apache.synapse.MessageContext;
@@ -23,37 +34,57 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.apache.synapse.commons.json.JsonUtil;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /*
 <class name="es.inetum.world.HazelcastMediator" >
 	<property name="method" value="GetSinglePricePerItemClientCallable" />
-	<property name="divisionId" value="374" />
-	<property name="clientId" value="1" />
-	<property name="productSKU" value="1" />
+	<property name="divisionId" value="001" />
+	<property name="clientId" value="001" />
+	<property name="productSKU" value="001" />
+	<property name="umv" value="0" />
 </class>
+<class name="es.inetum.world.HazelcastMediator" >
+	<property name="method" value="GetPricesPerClientNItemsCallable" />
+	<property name="divisionId" value="001" />
+	<property name="clientId" value="001" />
+	<property name="productSKU" value="001,999" />
+	<property name="umv" value="0" />
+</class>
+
+curl -X POST http://localhost:8280/hz
 */
 public class HazelcastMediator extends AbstractMediator implements ManagedLifecycle {
 
 	private static final Logger log = LoggerFactory.getLogger(HazelcastMediator.class);
-	private static final String DIV_1_ID = "374";
+	private static final String DIV_1_ID = "001";
 	
 	private static HazelcastInstance client = null;
 	private static IExecutorService exec = null;
+	private static IMap<BasicTariffKey, TariffProductPrice> basicTariffMap = null;
 
 	private String method;
 	private String divisionId;
 	private String clientId;
 	private String productSKU;
+	private int umv;
 
 	public boolean mediate(MessageContext context) { 
 		
-		//GetSinglePricePerItemClientCallable("374","1","1");
 		String body = null;
 		switch (method) {
 			case "GetSinglePricePerItemClientCallable":
-				body = GetSinglePricePerItemClientCallable(divisionId,clientId,productSKU);
+				body = GetSinglePricePerItemClientCallable(divisionId,clientId,productSKU,umv);
 				break;
+			case "GetPricesPerClientNItemsCallable":
+				body = GetPricesPerClientNItemsCallable(divisionId,clientId,productSKU,umv);
+				break;
+			/*
+			case "GetPricesTariffNItemsCallable":
+				body = GetPricesTariffNItemsCallable(divisionId,clientId,productSKU,umv);
+				break;
+			*/
 			default:
 				log.info("Método no reconocido");
 		}
@@ -67,8 +98,8 @@ public class HazelcastMediator extends AbstractMediator implements ManagedLifecy
 		return success;
 	}
 
-	private static String GetSinglePricePerItemClientCallable(String divisionId, String clientId, String productSKU) {
-		GetSinglePricePerItemClientCallable priceRequest = new GetSinglePricePerItemClientCallable(divisionId,clientId,productSKU);
+	private static String GetSinglePricePerItemClientCallable(String divisionId, String clientId, String productSKU, int umv) {
+		GetSinglePricePerItemClientCallable priceRequest = new GetSinglePricePerItemClientCallable(divisionId,clientId,productSKU,umv);
 		Future<NetProductPrice> retrievedPrice = exec.submitToKeyOwner(priceRequest, DIV_1_ID);
 		JSONObject jsonObject = null;
 		try {
@@ -80,11 +111,87 @@ public class HazelcastMediator extends AbstractMediator implements ManagedLifecy
 		}
 		return jsonObject.toString();
 	}
+
+	private static String GetPricesPerClientNItemsCallable(String divisionId, String clientId, String productSKU,int umv) {
+		List<String> productSKUCollection = Arrays.stream(productSKU.split(",")).map(String::trim).collect(Collectors.toList());
 	
+		GetPricesPerClientNItemsCallable priceRequest = new GetPricesPerClientNItemsCallable(divisionId,clientId, productSKUCollection,umv);
+		Future<List<NetProductPrice>> retrievedPrice = exec.submitToKeyOwner(priceRequest, DIV_1_ID);
+		
+		JSONObject jsonObject = null;
+		try {
+			log.info("**Retrieved Price = " + retrievedPrice.get());
+			jsonObject = new JSONObject();
+			JSONArray array = new JSONArray();
+			
+			for(NetProductPrice price : retrievedPrice.get()) {
+				if (checkNetProductPrice(price)) {
+			        JSONObject p = new JSONObject();
+			        
+			        // Discounts
+			        JSONObject discountsObject = new JSONObject();
+			        discountsObject.put("total", price.getDiscounts().getTotal());
+			        for (int i=0;i<price.getDiscounts().getPercentages().size();i++) {
+			        	discountsObject.put("discount"+i, price.getDiscounts().getPercentages().get(i));
+			        }
+			        JSONArray discountsArray = new JSONArray();
+			        discountsArray.put(discountsObject);
+			        p.put("discounts", discountsArray);
+			        
+			        // Others params
+			        p.put("currency", price.getCucd());
+			        p.put("item-no", price.getItno());
+			        p.put("net", price.getNet());
+			        p.put("sales", price.getSales());
+			        p.put("vat-amount", price.getVat());
+			        p.put("vat-percentage", price.getVat100());
+			        
+			        //add to prices array
+			        array.put(p);
+				}
+		    }
+			jsonObject.put("prices",array);
+		}
+		catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		return jsonObject.toString();
+	}
+	
+	private static Boolean checkNetProductPrice (NetProductPrice price) {
+		Boolean res = false;
+		if (price.getCucd()!=null && price.getDiscounts()!=null && price.getNet() != 0.0 && price.getSales() != 0.0 && price.getVat() != 0.0 && price.getVat100() != 0.0) {
+			res = true;
+		}
+		else {
+			log.info("Product '"+ price.getItno()+"' not found");
+		}
+		
+		return res;
+	}
+
+	/*
+	private static String GetPricesTariffNItemsCallable(String divisionId, String clientId, String productSKU) {
+		
+		Set<BasicTariffKey> multipleTariffKeys = new HashSet<>();
+		for (String product : productSKU.split(",")) {
+			multipleTariffKeys.add(new BasicTariffKey(DIV_1_ID, product));
+		}
+		JSONObject jsonObject = null;
+		try {
+			Map<BasicTariffKey, TariffProductPrice> tariff = basicTariffMap.getAll(multipleTariffKeys);
+			jsonObject = new JSONObject(tariff);
+		}
+		catch (Exception e) {
+			log.error("Error in HazelcastMediator.GetPricesTariffNItemsCallable ", e);
+		}
+		return jsonObject.toString();
+	}
+	*/
 	public void destroy() {
 		if (client != null) {
 			log.info("Shutting down hazelcast client.");
-			client.shutdown();
+			//client.shutdown();
 		}
 	}
 
@@ -96,6 +203,7 @@ public class HazelcastMediator extends AbstractMediator implements ManagedLifecy
 						log.info("Initializing Hazelcast instance.");
 						client = HazelcastClient.newHazelcastClient();
 						exec = client.getExecutorService("default");
+						basicTariffMap = client.getMap("price-tariffrate-cust");
 						
 					}catch (Exception e){
 						log.error("Exception occured while creating hazelcast client:", e);
@@ -139,4 +247,12 @@ public class HazelcastMediator extends AbstractMediator implements ManagedLifecy
 		this.productSKU = productSKU;
 	}
 	
+	public int getUmv() {
+		return umv;
+	}
+
+	public void setUmv(int umv) {
+		this.umv = umv;
+	}
+
 }
